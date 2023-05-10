5c3b2ea5177b1bf8c7734a97ca5ecf9306982a8d
Gwin14 <fabiospinheira@gmail.com>
2023-05-10 17:04:47 -0300

    prefabs botados aleatoriamente

---
+++ Assets/Scripts/TilemapSpawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PrefabData
+{
+    public GameObject prefab;
+    public int spawnCount;
+}
+
+public class TilemapSpawner : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public List<PrefabData> prefabs = new List<PrefabData>();
+
+    void Start()
+    {
+        foreach (var prefabData in prefabs)
+        {
+            int count = 0;
+
+            // percorre cada tile do Tilemap até que o número de instâncias seja alcançado
+            while (count < prefabData.spawnCount)
+            {
+                // obtém uma posição aleatória dentro do Tilemap
+                Vector3 randomPosition = new Vector3(Random.Range(tilemap.cellBounds.min.x, tilemap.cellBounds.max.x),
+                                                     Random.Range(tilemap.cellBounds.min.y, tilemap.cellBounds.max.y),
+                                                     tilemap.transform.position.z);
+
+                // verifica se o tile existe na posição gerada
+                Vector3Int tilePosition = tilemap.WorldToCell(randomPosition);
+                if (tilemap.HasTile(tilePosition))
+                {
+                    // cria uma instância do prefab na posição gerada
+                    Instantiate(prefabData.prefab, randomPosition, Quaternion.identity);
+                    count++;
+                }
+            }
+        }
+    }
+}
